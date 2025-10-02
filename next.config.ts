003a03a5ee2e6bac586c380bbef76dbc3cc5b583
shinataputra <71615861+shinataputra@users.noreply.github.com>
2025-10-02 10:34:17 +0700

    up

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  images: { unoptimized: true },
-  basePath: "/myland",
-  assetPrefix: "/myland",
+  // output: "export",
+  // images: { unoptimized: true },
+  // basePath: "/myland",
+  // assetPrefix: "/myland",
 };
 
 export default nextConfig;
